3180fc9af34916eff30358cfdb4a38431ca1cbb9
KietDC <kietdc@fpt.com>
2024-07-31 10:38:16 +0700

    style: :bento: Set up images and logos in assets folder

---
+++ src/assets/currencyIcons.ts
@@ -0,0 +1,69 @@
+import ampLUNA from "./logo/ampLUNA.svg";
+import ATOM from "./logo/ATOM.svg";
+import axlUSDC from "./logo/axlUSDC.svg";
+import BLUR from "./logo/BLUR.svg";
+import bNEO from "./logo/bNEO.svg";
+import BUSD from "./logo/BUSD.svg";
+import ETH from "./logo/ETH.svg";
+import EVMOS from "./logo/EVMOS.svg";
+import GMX from "./logo/GMX.svg";
+import IBCX from "./logo/IBCX.svg";
+import IRIS from "./logo/IRIS.svg";
+import KUJI from "./logo/KUJI.svg";
+import LSI from "./logo/LSI.svg";
+import LUNA from "./logo/LUNA.svg";
+import OKB from "./logo/OKB.svg";
+import OKT from "./logo/OKT.svg";
+import OSMO from "./logo/OSMO.svg";
+import RATOM from "./logo/rATOM.svg";
+import rSWTH from "./logo/rSWTH.svg";
+import STATOM from "./logo/stATOM.svg";
+import STEVMOS from "./logo/stEVMOS.svg";
+import STLUNA from "./logo/stLUNA.svg";
+import STOSMO from "./logo/stOSMO.svg";
+import STRD from "./logo/STRD.svg";
+import SWTH from "./logo/SWTH.svg";
+import USC from "./logo/USC.svg";
+import USD from "./logo/USD.svg";
+import USDC from "./logo/USDC.svg";
+import WBTC from "./logo/WBTC.svg";
+import wstETH from "./logo/wstETH.svg";
+import YieldUSD from "./logo/YieldUSD.svg";
+import ZIL from "./logo/ZIL.svg";
+
+const currencyIcons: { [key: string]: string } = {
+    ampLUNA,
+    ATOM,
+    axlUSDC,
+    USDC,
+    YieldUSD,
+    BLUR,
+    bNEO,
+    BUSD,
+    ETH,
+    EVMOS,
+    GMX,
+    IBCX,
+    IRIS,
+    KUJI,
+    LSI,
+    LUNA,
+    OKB,
+    OKT,
+    OSMO,
+    RATOM,
+    rSWTH,
+    STATOM,
+    STEVMOS,
+    STLUNA,
+    STOSMO,
+    STRD,
+    SWTH,
+    USC,
+    USD,
+    WBTC,
+    wstETH,
+    ZIL,
+};
+
+export default currencyIcons;
